39bed6972f167edc53becfe796a1d36da9ca7038
gauravbisen1 <gouravbisen123@gmail.com>
2026-01-14 15:37:27 +0530

    learning/error-handling

--- fetchAPI/script.js
+++ fetchAPI/script.js
@@ -1,9 +1,18 @@
 const btn = document.getElementById("loadBtn");
 const list = document.getElementById("userList");
+const statusList = document.getElementById("status");
 
-async function fetchUser(){
+async function fetchUser() {
     try {
+        statusList.textContent = "Loading users...";
+
         const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+        // Manual error handling for HTTP errors
+        if (!res.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+
         const data = await res.json();
 
         list.innerHTML = "";
@@ -12,8 +21,12 @@ async function fetchUser(){
             li.textContent = user.name;
             list.appendChild(li);
         });
+
+        statusList.textContent = "Users loaded successfully";
+
     } catch (error) {
         console.log(error);
+        statusList.textContent = "Failed to load users. Please try again.";
     }
 }
 
