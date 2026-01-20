e82a876a8c76bf624867f74f1ae09299a2ad4745
gauravbisen1 <gouravbisen123@gmail.com>
2026-01-20 12:47:14 +0530

    learning/oops-js

---
+++ OOPs_JS/notes.md
@@ -0,0 +1,28 @@
+# javascript and classes
+
+## OOP programming paradigm (it is a fundamental style or approach for structuring and solving problems in computer programming)
+
+## Object 
+- collection of properties and methods
+- eg. toLowerCase
+
+## why use OOP
+- code messing up
+- to fix and avoid it we use oop
+
+## part of OOP
+- Object literal
+
+- Constructor function
+- Prototypes
+- Classes
+- Instances (new, this)
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
+
+
+//this - current context
---
+++ OOPs_JS/oop.js
@@ -0,0 +1,61 @@
+//object literals
+const user = {
+    username : "Gaurav",
+    loginCount : 8,
+    signedIn : true,
+
+    getUserDetails : function(){
+        // console.log("Got user details from DB!");
+        console.log(`Username: ${this.username}`); 
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+
+//CONSTRUCTOR FUNCTIONS
+
+//new - is constructor function - to create multiple instances from object literals
+
+//const promiseOne = new Promise(); // -> new to create new context
+//const date = new Date();
+
+// constructor function always give new instance, new copy
+// orignal and others cant effect from it
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username; // inside func variable = passed value in function
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    //methos also used
+    this.greeting = function(){
+        console.log(`Welcome ${this.username} !`);
+    }
+
+    // return this; implicitly defiened dont want to use
+    return this;
+}
+
+const userOne = new User("gaurav", 21, true);
+const userTwo = new User("bisen", 25, false); // all value override in previous user
+
+console.log(userOne);
+console.log(userOne.constructor); // gives user function, cusnstructor is  reference of our own
+console.log(userTwo);
+console.log(userOne.greeting());
+
+//New keyword -
+// when we use new keyword, an empty object first created which is called instance.  
+
+//1- new object created
+//2- constructor function called beacause of new
+//3- this keyword -  argument inject inside it
+//4- found inside function
+
+//instanceof - to se if the prototype property of a constructor appears anywhere in the prototype chain of an bject.
+//the return value is a boolean value
+
+
