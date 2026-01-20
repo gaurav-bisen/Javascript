9cb5bef17d3a9255cb50dd6172bd31a9601b2a74
gauravbisen1 <gouravbisen123@gmail.com>
2026-01-20 14:39:46 +0530

    practice/add more features

--- To-Do-List-App/script.js
+++ To-Do-List-App/script.js
@@ -1,32 +1,54 @@
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 const list = document.getElementById("list");
+const doneList = document.getElementById("doneTasks");
 
 let tasks = JSON.parse(localStorage.getItem("myTasks")) || [];
+let doneTasks = JSON.parse(localStorage.getItem("doneTasks")) || [];
 
 function allTasks() {
     list.innerHTML = ''; //clear list first (causing duplicates)
-    tasks.forEach((task,idx) => {
+    tasks.forEach((task, idx) => {
         const li = document.createElement('li');
         // li.innerHTML = `• ${task} <button onclick="deleteTask(${idx})">delete</button>`;
-        li.innerHTML = `${task} <a href="#" onclick="deleteTask(${idx})"><i class="fa-solid fa-trash-can"></i></a>`;
+        li.innerHTML = `${task} <a href="#" onclick="deleteTask(${idx})"><i class="fa-regular fa-square-check"></i></a>`;
         list.appendChild(li);
     });
 }
 
+function allDoneTasks() {
+    doneList.innerHTML = ''
+    doneTasks.forEach((task, idx) => {
+        const li = document.createElement('li');
+        li.innerHTML = `${task} <a href="#" onclick="deleteDoneTask(${idx})"><i class="fa-solid fa-trash-can"></i></a>`;
+        doneList.appendChild(li);
+
+    })
+}
 
 function saveAll() {
     localStorage.setItem("myTasks", JSON.stringify(tasks));
     allTasks();
 }
 
-function deleteTask(idx){
-    tasks.splice(idx,1);
+function deleteTask(idx) {
+    const doneTask = tasks.splice(idx, 1);
+    doneTasks.push(doneTask);
+
+
+    localStorage.setItem("doneTasks", JSON.stringify(doneTasks));
     saveAll();
+    allDoneTasks();
+}
+
+function deleteDoneTask(idx) {
+    doneTasks.splice(idx, 1);
+    localStorage.setItem("doneTasks", JSON.stringify(doneTasks));
+    allDoneTasks();
 }
 
 
-btn.addEventListener("click", ()=>{
+btn.addEventListener("click", () => {
     const text = input.value;
     if (text) {
         tasks.push(text);
@@ -36,3 +58,4 @@ btn.addEventListener("click", ()=>{
 });
 
 allTasks();
+allDoneTasks();
