f93f5c32b1e044e71716d85558bfbe5d537a4f7e
gauravbisen1 <gouravbisen123@gmail.com>
2026-01-21 15:04:38 +0530

    task/feature-added

--- WeatherAPI/getWeather.js
+++ WeatherAPI/getWeather.js
@@ -1,7 +1,7 @@
 async function getWeather(data) {
 
-    const city = cityInput.value;
-    if (!city) return;
+    // const city = cityInput.value;
+    // if (!city) return;
 
     cityName.textContent = data.location.name + ", " + data.location.country;
     temp.textContent = data.current.temp_c;
